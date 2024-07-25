a2d6539fbb9fa26036916265cf1c80e37523db7b
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-24 21:07:57 -0300

    Add: Backdrop filter params

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import ParamSelector from './components/paramControl';
 
 function App() {
   const [cssValues, setCssValues] = useState({
-    borderRadius: 0,
-    width: 200,
-    height: 100,
-    backgroundColor: 'lightblue'
+    borderRadius: 8,
+    blur: 0,
+    saturation: 0,
+    opacity: 100
   });
 
   const handleCssChange = (property, value) => {
@@ -25,7 +25,15 @@ function App() {
       <ToolBar />
       <div className='d-flex flex-row app-main-frame' id='app-box'>
         <div>
-          <div className='card' id='app-card' style={{ borderRadius: `${cssValues.borderRadius}px`}}
+          <div 
+            className='card' 
+            id='app-card' 
+            style={{ 
+              borderRadius: `${cssValues.borderRadius}px`, 
+              opacity: `${cssValues.opacity}%`,
+              backdropFilter: `blur(${cssValues.blur}px) saturation(${`cssValues.saturaion`}%)`
+
+            }}
           >Conteúdo Teste</div>
         </div>
         <div>
@@ -37,6 +45,33 @@ function App() {
             setId="borderRadiusRange" 
             property="borderRadius" 
             value={cssValues.borderRadius} 
+            handleCssChange={handleCssChange} 
+          />
+          <ParamSelector 
+            label="Saturação do Fundo"
+            min={0} 
+            max={250} 
+            setId="saturationRange" 
+            property="saturation" 
+            value={cssValues.saturation} 
+            handleCssChange={handleCssChange} 
+          />
+          <ParamSelector 
+            label="Desfoque do Fundo"
+            min={0} 
+            max={100} 
+            setId="blurRange" 
+            property="blur" 
+            value={cssValues.blur} 
+            handleCssChange={handleCssChange} 
+          />
+          <ParamSelector 
+            label="Opacidade"
+            min={0} 
+            max={100} 
+            setId="opacityRange" 
+            property="opacity" 
+            value={cssValues.opacity} 
             handleCssChange={handleCssChange} 
           />
         </div>
